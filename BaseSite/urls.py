2fd755da2bbb3953dc3c90e3f75057ee8d144519
Pouria Velaei <77537743+pouriavelaei@users.noreply.github.com>
2024-04-24 09:19:41 +0330

    new templates
    delete old template and add new template

--- BaseSite/urls.py
+++ BaseSite/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, profile
+
+app_name="BaseSite"
+
 urlpatterns = [
-    path('', home)
+    path('', home, name="home"),
+    path("/profile", profile, name="profile"),
 ]
--- BaseSite/views.py
+++ BaseSite/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def profile(request):
+    return render(request, 'profile.html')
