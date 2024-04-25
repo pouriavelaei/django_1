d3cc1f3f09356e971e247b308708193eb96d0284
Pouria Velaei <77537743+pouriavelaei@users.noreply.github.com>
2024-04-25 11:53:54 +0330

    extands file with base.html

--- BaseSite/views.py
+++ BaseSite/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'base.html')
+
 
 def explore(request):
     return render(request, 'explore.html')
 
+
 def details(request):
     return render(request, 'details.html')
+
 
 def create(request):
     return render(request, 'create.html')
 
+
 def author(request):
     return render(request, 'author.html')
-
